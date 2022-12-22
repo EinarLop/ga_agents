2b9b18e725ce7529bfe5a1f5517bffa7860ce384
EinarLop <einarlop0@gmail.com>
2022-12-21 20:14:29 -0600

    GA working with three targets

--- ball.py
+++ ball.py
@@ -2,24 +2,32 @@ import random
 import math
 import pyglet
 from pyglet import shapes
+import numpy as np
 
-generation_size = 20
-gens_size = 500
-
+generation_size = 100
+gens_size = 100
+number_generations = 10000
 
 batch = pyglet.graphics.Batch()
+
+stars = [shapes.Star(100, 50, 5, 10, num_spikes=5, color=(255, 255, 0), batch=batch), shapes.Star(250, 300, 5, 10, num_spikes=5, color=(255, 255, 0), batch=batch)]
+target = shapes.Rectangle(375, 190, 20, 20, color=(255, 22, 20), batch=batch)
+
 
 
 class Ball:
     def __init__(self):
         self.movements = []
-        self.agent = shapes.Circle(200, 200, 5, color=(random.randint(0,255),random.randint(0,255),random.randint(0,255)), batch=batch)
+        self.agent = shapes.Circle(-5, 200, 5,
+                                   color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+                                   batch=batch)
         self.fitness = 0
-        self.reward = False
-    
-    def create_gens(self, parentOne = [], parentTwo = []):
+        self.rewards = [False, False]
+        self.target = False
+
+    def create_gens(self, parentOne=[], parentTwo=[]):
         if len(parentOne) == 0:
-            self.movements = [[random.randint(-1,1), random.randint(-1,1)] for _ in range(gens_size)]
+            self.movements = [[0,0] for _ in range(gens_size)]
             # print("first gen", self.movements)
 
         else:
@@ -27,27 +35,32 @@ class Ball:
             self.movements = parentOne[0:crossPoint] + parentTwo[crossPoint:]
             # print("new gens", self.movements)
 
-
     def move(self, frame):
         self.agent.x += self.movements[frame][0]
         self.agent.y += self.movements[frame][1]
-    
+
     def calculate_fitness(self):
-        self.fitness += math.dist([self.agent.x,self.agent.y], [340,50]) * 150
+        # self.fitness += (-10 * math.dist([self.agent.x, self.agent.y], [target.x, target.y])) + (100*len(np.where(self.rewards == False)))
+        self.fitness += math.dist([self.agent.x, self.agent.y], [target.x, target.y])
+        pass
 
     def mutate(self):
-        for _ in range(random.randint(0, 10)):
-            self.movements[random.randint(0, gens_size)-1] = [random.randint(-5,5), random.randint(-5,5)] 
+        self.movements[random.randint(0, gens_size) - 1] = [random.randint(-20, 20), random.randint(-20, 20)]
 
     def star_collision(self):
-        distance = math.dist([self.agent.x,self.agent.y], [350,350])
-        if distance <= 10 and not self.reward:
+        for i, star in enumerate(stars):
+            agenetTargetDistance = math.dist([self.agent.x, self.agent.y], [star.x, star.y])
+            starTargetDistance = math.dist([star.x, star.y], [target.x, target.y])
+            agentStarDistance = math.dist([star.x, star.y], [self.agent.x, self.agent.y])
+            if agentStarDistance <= 10 and not self.rewards[i]:
+                self.fitness -= 250
+                self.rewards[i] = True
+            if agenetTargetDistance <= 10 and not self.target:
+                self.fitness -= 500
+                self.target = True
+            if not self.rewards[i]:
+                self.fitness += agentStarDistance*0.05
+            if not self.target:
+                self.fitness += agenetTargetDistance*0.1
 
-            self.fitness -= 100
-            self.reward = True
-        elif distance>10 and not self.reward:
-    
-            self.fitness = self.fitness - (900 - distance)
-            
-            
-        
+
--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import pyglet
 from pyglet import shapes
 import random
 from ball import *
+import matplotlib.pyplot as plt
 
-generation_size = 20
-gens_size = 500
 
-window = pyglet.window.Window(400,400)
-target = shapes.Rectangle(340, 50, 20, 20, color=(255, 22, 20), batch=batch)
+window = pyglet.window.Window(400, 600)
+
 
 balls = []
 frame = 0
 fitness_history = []
+currentGenNum = 0
 
-label = pyglet.text.Label("",
-                          font_name='Times New Roman',multiline=True, width =150, height=400,
-                          font_size=10,color=(255, 255,255,255),
-                          x=0, y=390)  
-star = shapes.Star(350, 350, 5, 10, num_spikes=5, color=(255, 255, 0), batch=batch)
+
+graph = pyglet.graphics.Batch()
+points = []
+
+numGenLabel = pyglet.text.Label("Generation: " + str(currentGenNum),
+                                font_size=10,
+                                color=(255, 255, 255, 255),
+                                x=150,
+                                y=380)
 
 for _ in range(generation_size):
     currentBall = Ball()
     currentBall.create_gens()
     balls.append(currentBall)
-
 
 
 def init_gen(parentOne, parentTwo):
@@ -33,56 +36,61 @@ def init_gen(parentOne, parentTwo):
     for _ in range(generation_size):
         currentBall = Ball()
         currentBall.create_gens(parentOne, parentTwo)
-        if random.random() > 0.85:
-            currentBall.mutate()
+
+        currentBall.mutate()
         balls.append(currentBall)
     frame = 0
-
-    
-    pyglet.clock.schedule_interval(life, 0.001)
+    pyglet.clock.schedule_interval(life, 0.01)
 
 
 def select_parents():
     for ball in balls:
         ball.calculate_fitness()
     balls.sort(key=lambda x: x.fitness, reverse=False)
-    return balls[0] ,balls[1]
-    
+    return balls[0], balls[1]
+
 
 def life(dt):
-    
     global frame
-    
+    global currentGenNum
+    global numGenLabel
 
     for ball in balls:
         ball.agent.x += ball.movements[frame][0]
         ball.agent.y += ball.movements[frame][1]
         ball.star_collision()
-   
+
+    # if currentGenNum > number_generations:
+    #     pyglet.clock.unschedule(life)
     if frame >= gens_size-1:
         pyglet.clock.unschedule(life)
-        parentOne, parentTwo = select_parents()
-        init_gen(parentOne.movements,parentTwo.movements)
-        fitness_history.append((parentOne.fitness+parentTwo.fitness)/2)
-    global label
-
-    label.text = "Best parent fitness history: " + ' '.join(str(v) for v in fitness_history) 
-    
-
-   
+        if currentGenNum < number_generations:
+            parentOne, parentTwo = select_parents()
+            currentFitness = (parentOne.fitness + parentTwo.fitness) / 2
+            points.append(shapes.Circle(5+currentGenNum, 400+ currentFitness, 1, color=(255, 0, 0), batch=graph))
+            print("Current fitness", parentOne.movements)
+            fitness_history.append(currentFitness)
+            init_gen(parentOne.movements, parentTwo.movements)
+            numGenLabel.text = "Generation: " + str(currentGenNum)
+            currentGenNum += 1
     frame += 1
 
-
-
-
-
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+    valueScaled = float(value - leftMin) / float(leftSpan)
+    return rightMin + (valueScaled * rightSpan)
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-    label.draw()
+    numGenLabel.draw()
+    graph.draw()
 
-pyglet.clock.schedule_interval(life, 0.001)
+
+pyglet.clock.schedule_interval(life, 0.01)
 
 pyglet.app.run()
+plt.plot(fitness_history)
+plt.show()
